6113c283d7788c9c7f6361b16a1bd01c1f894360
chinmayee <chinmayee@chinmayees-MacBook-Air.local>
2025-01-06 09:14:22 +0530

    Initial Commit

--- 18.py
+++ 18.py
@@ -1,2 +1,7 @@
 print("hi")
-print("exam")
+print("git init
+git add hi.py 
+git commit -m  "Initial Commit"
+git branch
+git remote add origin https://github.com/username/<repository-name>.git
+git push -u origin master")
