ae8439c0830e0814fb157940d3320bc81998c2fe
chinmayee <chinmayee@chinmayees-MacBook-Air.local>
2025-01-06 09:15:56 +0530

    Initial Commit

--- 18.py
+++ 18.py
@@ -5,3 +5,8 @@ git commit -m  "Initial Commit"
 git branch
 git remote add origin https://github.com/username/<repository-name>.git
 git push -u origin master")
+print("FROM python:3.9
+COPY hi.py .
+CMD ["python","hi.py"]
+docker build -t hi.py
+docker run hi.py")
